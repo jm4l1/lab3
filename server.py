e214a029a00ec41def4d99605cf57b25b624b211
Aidan Newallo <aidannewallo@gmail.com>
2023-10-15 21:40:17 -0400

    816037521

--- server.py
+++ server.py
@@ -59,6 +59,12 @@ class handler(BaseHTTPRequestHandler):
         # set response code
         self.send_response(403)
 
+        #read content length header
+        content_len = int(self.headers.get('Content-Length'))
+        # get content-body
+        post_body = self.rfile.read(content_len)
+        post_body = json.loads(post_body)
+
         # set headers
         self.send_header('content-type', 'text/html')
         self.end_headers()
